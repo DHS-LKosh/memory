430e9cd78355336d3dca1dccf050201bb69a84e1
Ms. Kosh <119891028+DHS-LKosh@users.noreply.github.com>
2023-09-22 09:22:33 -0400

    Update App.java
    Default template for assignment

--- src/App.java
+++ src/App.java
@@ -1,5 +1,33 @@
 class App{
+  //'global' variables
+  static int[] cards;
+  static boolean[] isShown;
+  
   public static void main(String[] args){
+    //Game loop goes here
+    //User input
+    
+  }
 
+  public static void displayBoard(){
+
+  }
+
+  //overloaded displayBoard
+  public static void displayBoard(int r, int c){
+
+  }
+
+  //overloaded displayBoard
+  public static void displayBoard(int r, int c, int i, int j){
+
+  }
+
+  public static void shuffleBoard(){
+
+  }
+
+  public static boolean isDone(){
+    return false;
   }
 }
